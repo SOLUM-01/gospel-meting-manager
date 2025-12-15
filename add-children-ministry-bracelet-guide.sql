fd1f52ce25be8be8b609bd3b83c5a6fa0004c81a
solum <kjh170111@gmail.com>
2025-12-15 11:02:44 +0900

    feat: add 6 YouTube videos to children ministry bracelet guide

--- add-children-ministry-bracelet-guide.sql
+++ add-children-ministry-bracelet-guide.sql
@@ -1,9 +1,17 @@
--- 어린이 사역팀에 복음 팔찌 가이드 이미지 추가
+-- 어린이 사역팀에 복음 팔찌 가이드 이미지 및 유튜브 동영상 추가
 -- 실행 방법: Supabase 대시보드 > SQL Editor에서 실행
 
 UPDATE tasks
 SET 
-  images = ARRAY['/images/gospel-bracelet-guide.png'],
+  images = ARRAY[
+    '/images/gospel-bracelet-guide.png',
+    'https://www.youtube.com/embed/hUX5VKVj-t8',
+    'https://www.youtube.com/embed/Ae3ZjPE4UG8',
+    'https://www.youtube.com/embed/ZA64sU4f2FI',
+    'https://www.youtube.com/embed/-I6F-J3yOUI',
+    'https://www.youtube.com/embed/TjDrB7cM7Go',
+    'https://www.youtube.com/embed/fpBRDbQK7KQ'
+  ],
   updated_at = NOW()
 WHERE id = 'cc0bfb98-b384-46d3-91c1-89fed97eb7e3';
 
